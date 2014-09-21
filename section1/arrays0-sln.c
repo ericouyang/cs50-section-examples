b48b59e54fdbff0ee448f90f566a0a536f8884eb
Eric Ouyang <contact@ericouyang.com>
2014-09-21 11:37:34 -0400

    add section1 arrays examples

---
+++ section1/arrays0-sln.c
@@ -0,0 +1,24 @@
+/**
+ * arrays0.c - Initializing arrays (Solution)
+ *
+ * Computer Science 50
+ * Section 1
+ *
+ * Eric Ouyang <eouyang@college.harvard.edu>
+ * Frederick Widjaja <fwidjaja@college.harvard.edu>
+ *
+ * Let's declare some arrays
+ */
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void) {
+    // create the integer array with elements {65, 87, 30}
+    int nums[3];
+    nums[0] = 63;
+    nums[1] = 87;
+    nums[2] = 30;
+
+    // create the same array in shorthand
+    int nums[] = {63, 87, 30};
+}
---
+++ section1/arrays0.c
@@ -0,0 +1,19 @@
+/**
+ * arrays0.c - Initializing arrays
+ *
+ * Computer Science 50
+ * Section 1
+ *
+ * Eric Ouyang <eouyang@college.harvard.edu>
+ * Frederick Widjaja <fwidjaja@college.harvard.edu>
+ *
+ * Let's declare some arrays
+ */
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void) {
+    // create the integer array with elements {65, 87, 30}
+
+    // create the same array in shorthand
+}
