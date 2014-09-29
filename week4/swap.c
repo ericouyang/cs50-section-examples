c03ef9e5b42612b671257ad26628a2e8bd02e54b
Eric Ouyang <contact@ericouyang.com>
2014-09-29 19:40:37 -0400

    add swap example

---
+++ week4/swap.c
@@ -0,0 +1,34 @@
+/**
+ * selection-sort0.c - Selection sort
+ *
+ * Computer Science 50
+ * Week 4
+ *
+ * Eric Ouyang <eouyang@college.harvard.edu>
+ * Frederick Widjaja <fwidjaja@college.harvard.edu>
+ *
+ * Implement selection sort!
+ */
+#include <stdio.h>
+#include "helpers.h"
+
+int main(void)
+{
+    int nums[] = {1, 2, 3, 5, 4};
+    int size = 5;
+
+    print_array(nums, size);
+
+    // the lines below perform the swap of elements 3 and 4
+
+    // save the value in position 3
+    int tmp = nums[3];
+
+    // set the value in position 3 to the value from position 4
+    nums[3] = nums[4];
+
+    // set the value in position 4 to the previously saved value
+    nums[4] = tmp;
+
+    print_array(nums, size);
+}
