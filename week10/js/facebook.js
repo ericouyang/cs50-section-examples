8edbc064d46ee4ca3300f896cd8dacf71f58fb59
Eric Ouyang <ericouyang@gmail.com>
2015-11-09 19:20:29 +0000

    update week10 example

--- week10/js/facebook.js
+++ week10/js/facebook.js
@@ -1,17 +1,12 @@
-// return a Promise with the result of the AJAX request
-// to the Facebook Graph API for user details
 
-// see https://developers.facebook.com/docs/graph-api/reference/v2.2/user
-function getFbUser(username) {
-    console.log("Looking up " + username + " using the Graph API...");
+// https://developers.facebook.com/docs/graph-api/reference/v2.5/user
+function getCurrentUser(cb) {
+    console.log("Looking up current user using the Graph API...");
     // TODO
 }
 
-// return a Promise with the result of the AJAX request
-// to the Facebook Graph API for user profile picture
-
-// see https://developers.facebook.com/docs/graph-api/reference/v2.2/user/picture
-function getFbProfilePicture(username) {
+// see https://developers.facebook.com/docs/graph-api/reference/v2.5/user/picture
+function getFbProfilePicture(cb) {
     // TODO
 }
 
@@ -20,12 +15,55 @@ function updateDisplay(user) {
     // TODO
 }
 
+function clearDisplay() {
+    // TODO
+}
+
 // display an error to the user through an alert box
 function showError() {
     alert("Oops! There was an error loading the user details.");
 }
 
+// displays the current user
+function displayCurrentUser() {
+    // TODO
+}
+
+// callback to respond to login state
+// see https://developers.facebook.com/docs/reference/javascript/FB.getLoginStatus
+function fbCallback(response) {
+    console.log(response);
+
+    // TODO
+}
+
+
 // set up the website with the proper event handler
 $(document).ready(function() {
-    // TODO
+    // see https://developers.facebook.com/docs/facebook-login/web
+    window.fbAsyncInit = function() {
+        FB.init({
+          appId      : '479509265564676',
+          xfbml      : true,
+          version    : 'v2.5'
+        });
+        
+        // Now that we've initialized the JavaScript SDK, we call 
+        // FB.getLoginStatus().  This function gets the state of the
+        // person visiting this page and can return one of three states to
+        // the callback you provide.  They can be:
+        //
+        // 1. Logged into your app ('connected')
+        // 2. Logged into Facebook, but not your app ('not_authorized')
+        // 3. Not logged into Facebook and can't tell if they are logged into
+        //    your app or not.
+        //
+        // These three cases are handled in the callback function.
+        
+        FB.getLoginStatus(fbCallback);
+        
+        // subscribe to events
+        FB.Event.subscribe('auth.login', fbCallback);
+        FB.Event.subscribe('auth.logout', fbCallback);
+    };
 });
--- week10/js/facebook_sln.js
+++ week10/js/facebook_sln.js
@@ -1,28 +1,20 @@
-// return a Promise with the result of the AJAX request
-// to the Facebook Graph API for user details
 
-// see https://developers.facebook.com/docs/graph-api/reference/v2.2/user
-function getFbUser(username) {
-    console.log("Looking up " + username + " using the Graph API...");
-    return $.getJSON("http://graph.facebook.com/" + username);
+// https://developers.facebook.com/docs/graph-api/reference/v2.5/user
+function getCurrentUser(cb) {
+    console.log("Looking up current user using the Graph API...");
+    FB.api('/me', {fields: ['email', 'first_name', 'last_name']}, cb);
 }
 
-// return a Promise with the result of the AJAX request
-// to the Facebook Graph API for user profile picture
-
-// see https://developers.facebook.com/docs/graph-api/reference/v2.2/user/picture
-function getFbProfilePicture(username) {
-    return $.getJSON("http://graph.facebook.com/" + username + "/picture", {
-        redirect: 0,
-        type: "large"
-    });
+// see https://developers.facebook.com/docs/graph-api/reference/v2.5/user/picture
+function getFbProfilePicture(cb) {
+    FB.api('/me/picture', {type: 'large'}, cb);
 }
 
 // update the display with the given user
 function updateDisplay(user) {
     // update user details
     $("#fb-id").text(user.id);
-    $("#fb-username").text(user.username);
+    $("#fb-email").text(user.email);
     $("#fb-first-name").text(user.first_name);
     $("#fb-last-name").text(user.last_name);
 
@@ -33,30 +25,75 @@ function updateDisplay(user) {
     $("#fb-image").html(image);
 }
 
+function clearDisplay() {
+    $("#fb-id").empty();
+    $("#fb-email").empty();
+    $("#fb-first-name").empty();
+    $("#fb-last-name").empty();
+    $("#fb-image").empty();
+}
+
 // display an error to the user through an alert box
 function showError() {
     alert("Oops! There was an error loading the user details.");
 }
 
+// displays the current user
+function displayCurrentUser() {
+    getCurrentUser(function(user){
+        getFbProfilePicture(function(data){
+            user.picture = data.data.url;
+            updateDisplay(user);
+        });
+    });
+}
+
+// callback to respond to login state
+// see https://developers.facebook.com/docs/reference/javascript/FB.getLoginStatus
+function fbCallback(response) {
+    console.log(response);
+
+    if (response.status === 'connected') {
+        $("#fb-status").text("You're now logged in!");
+        displayCurrentUser();
+        return;
+    }
+    else if (response.status === 'not_authorized') {
+        $("#fb-status").text("Please log into this app!");
+    } 
+    else {
+        $("#fb-status").text("Please log into Facebook!");
+    }
+    clearDisplay();
+}
+
+
 // set up the website with the proper event handler
 $(document).ready(function() {
-    $("form").submit(function(e){
-        e.preventDefault();
-        var username = $("#username").val();
-        if (username === "") {
-            alert("You must enter in the username you want to lookup!");
-        }
-        else {
-            getFbUser(username)
-                .done(function(user){
-                    getFbProfilePicture(username)
-                        .done(function(data){
-                            user.picture = data.data.url;
-                            updateDisplay(user);
-                        })
-                        .fail(showError);
-                })
-                .fail(showError);
-        }
-    });
+    // see https://developers.facebook.com/docs/facebook-login/web
+    window.fbAsyncInit = function() {
+        FB.init({
+          appId      : '479509265564676',
+          xfbml      : true,
+          version    : 'v2.5'
+        });
+        
+        // Now that we've initialized the JavaScript SDK, we call 
+        // FB.getLoginStatus().  This function gets the state of the
+        // person visiting this page and can return one of three states to
+        // the callback you provide.  They can be:
+        //
+        // 1. Logged into your app ('connected')
+        // 2. Logged into Facebook, but not your app ('not_authorized')
+        // 3. Not logged into Facebook and can't tell if they are logged into
+        //    your app or not.
+        //
+        // These three cases are handled in the callback function.
+        
+        FB.getLoginStatus(fbCallback);
+        
+        // subscribe to events
+        FB.Event.subscribe('auth.login', fbCallback);
+        FB.Event.subscribe('auth.logout', fbCallback);
+    };
 });
